224f446924f36e08f5c63fefd4e890cef67c6bca
Ryan Tomlinson <ryantomlinson95@gmail.com>
2016-03-07 14:37:31 -0500

    Initialized xcode project

---
+++ iCheckers/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  iCheckers
+//
+//  Created by Ryan Tomlinson on 3/7/16.
+//  Copyright (c) 2016 Lockness. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
